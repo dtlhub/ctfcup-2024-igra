b595b4ac6468a15108d8af1add1b9d854e353131
Alexey Tarasov <tarasovion2004@gmail.com>
2024-12-22 10:04:57 +0300

    remote address

--- scripts/client.sh
+++ scripts/client.sh
@@ -7,14 +7,13 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-LEVEL="test"
+LEVEL="level-1"
 
 ADDRESS="localhost:8085"
-if [ "$1" == "local" ]; then
-    ADDRESS="localhost:8085"
-else
-    echo "TODO: set remote address"
-    exit 1
+AUTH_TOKEN="dtl:12345"
+if [ "$1" == "remote" ]; then
+    ADDRESS="172.16.30.195:13372"
+    AUTH_TOKEN="dtl:509bd7cc468edebcc2090088ea01b43116b4f3f68b993e4ec68652653c0281d7"
 fi
 
-AUTH_TOKEN=dtl:12345 go run cmd/client/main.go -s "$ADDRESS" -l "$LEVEL"
+AUTH_TOKEN="$AUTH_TOKEN" go run cmd/client/main.go -s "$ADDRESS" -l "$LEVEL"
--- scripts/server.sh
+++ scripts/server.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-LEVEL="test"
+LEVEL="level-1"
 
 AUTH_TOKEN=dtl:12345 go run cmd/server/main.go -s ":8085" -h -l "$LEVEL"
